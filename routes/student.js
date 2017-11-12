2809d6b2228c246177661889a18f0a86f3eb02b3
óluchi@kodedly <meetenebelioluchi@gmail.com>
2017-11-12 18:02:33 +0000

    cleared a bug in the route file

--- routes/student.js
+++ routes/student.js
@@ -53,7 +53,7 @@ const upload = multer({
 }); 
 // router.use(sanitizer());
 
-paramSanitizer.sanitizeParams(router, ['id']);
+// paramSanitizer.sanitizeParams(router, ['id']);
 /* GET all students request handler. */
 router.use(paginate.middleware(10, 10));
 router.get('/', StudentController.listStudent);
